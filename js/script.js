31adecf1351a8aa387a44ee8b752e3bdb82d73fc
alexomaset <alexomaset711@gmail.com>
2019-02-18 14:04:45 +0300

    modified css and js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-var triangleTracker = function(sideA, sideB, sideC) {
-    return sideA, sideB, sideC;
-    }
-    var sideA=parseInt(prompt("input side A"));
-    var sideB=parseInt(prompt("input side B"));
-    var sideC=parseInt(prompt("input side C"));
-     if(sideA+sideB<=sideC&&sideA+sideC<=SideB&&sideC+sideB<=sideA) {
-     alert("NO triangle");
-     } else if(sideA===sideB&&sideB===sideC) {
-     alert("Equilateral Triangle")
-     } else if(sideA===sideB&&sideB!==sideA||sideB===sideC&&sideC!==sideA) {
-     alert("Isosceles Triangle")
-     } else if(sideA!==sideB&&sideB!==sideC&&sideC!==sideA&&sideA+sideB>sideC&&sideA+sideC>sideB&&sideC+sideB>sideA) {
-     alert("Scalene Triangle")
-     } else {
-     alert("NO Triangle")
-     }
+function tap() {
+var sidea = document.getElementById("data1").value;
+var sideb = document.getElementById("data2").value;
+var sidec = document.getElementById("data3").value;
+var sideA=parseInt(sideA);
+var sideB=parseInt(sideB);
+var sideC=parseInt(sideC);
+console.log(sideA)
+console.log(sideB)
+console.log(sideC)
+if(sideA+sideB<sideC&&sideA+sideC<sideB&&sideC+sideB<sideA) {
+  alert("NO triangle");
+} else if(sideA===sideB&&sideB===sideC) {
+  alert("Equilateral Triangle");
+} else if(sideA===sideB||sideB===sideC||sideA===sideC) {
+  alert("Isosceles Triangle");
+} else if(sideA!==sideB&&sideB!==sideC&&sideC!==sideA&&sideA+sideB>sideC&&sideA+sideC>sideB&&sideC+sideB>sideA) {
+  alert("Scalene Triangle");
+} else {
+  alert("NO Triangle");
+}
+}
