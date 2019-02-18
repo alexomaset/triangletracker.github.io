57d30675e37053b958db8951366881f2a120420b
alexomaset <alexomaset711@gmail.com>
2019-02-18 08:11:53 +0300

    Add iniitial changes to css, indexand js files

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
-var triangleTracker = function(side1, side2, side3) {
-	if(((side1 + side2) >= side3) && ((side2 + side3) >= side1) && ((side3 + side1) && side2)) { 
-        return typeOfTriangle(side1, side2, side3); 
-
-var typeOfTriangle = function(side1, side2, side3) {
-	if((side1 === side2) && (side2 === side3)) {
-		return "equilateral triangle"
-	} else if((sideA === sideB) || (sideC === side) || (side2 === side3)) {
-		return "isoceles triangle"
-	} else {
-		return "scalene triangle"
-	}
-};
-$("input.formbtn").click(function(event) {
-    var side1 = parseInt($("#side1").val());
-    var side2 = parseInt($("#side2").val());
-    var side3 = parseInt($("#side3").val());
-    var show = triangle(sideA, sideB, sideC);
-};
+var triangleTracker = function(sideA, sideB, sideC) {
+    return sideA, sideB, sideC;
+    }
+    var sideA=parseInt(prompt("input side A"));
+    var sideB=parseInt(prompt("input side B"));
+    var sideC=parseInt(prompt("input side C"));
+     if(sideA+sideB<=sideC&&sideA+sideC<=SideB&&sideC+sideB<=sideA) {
+     alert("NO triangle");
+     } else if(sideA===sideB&&sideB===sideC) {
+     alert("Equilateral Triangle")
+     } else if(sideA===sideB&&sideB!==sideA||sideB===sideC&&sideC!==sideA) {
+     alert("Isosceles Triangle")
+     } else if(sideA!==sideB&&sideB!==sideC&&sideC!==sideA&&sideA+sideB>sideC&&sideA+sideC>sideB&&sideC+sideB>sideA) {
+     alert("Scalene Triangle")
+     } else {
+     alert("NO Triangle")
